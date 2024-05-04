336bc1760dc48c6d2f7881c0810e024ce7ce1f0a
andre-dev-git <andre.carvalhobjj2002@gmail.com>
2024-05-03 23:35:43 -0300

    remove dist

--- dist/hello.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { HelloService } from './hello.service';
-export declare class HelloController {
-    private service;
-    constructor(service: HelloService);
-    hello(): string;
-}
--- dist/people/people.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { Person } from './person';
-export declare class PeopleService {
-    people: Person[];
-    list(): Person[];
-    findById(id: number): Person;
-    save(person: Person): void;
-    update(person: Person): boolean;
-    delete(personId: number): boolean;
-}
